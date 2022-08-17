dac613a6dd515819f9042e12332b52c731f1c35f
Valery <valery2003lvov@yandex.ru>
2022-08-17 20:20:29 +0300

    better links scroll

--- src/js/script.js
+++ src/js/script.js
@@ -17,3 +17,14 @@ menuItem.forEach((item) => {
 })
 
 
+$(document).ready(function(){
+    $("a[href*='#']").on("click", function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 777);
+        e.preventDefault();
+        return false;
+    });
+});
+
